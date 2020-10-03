d102e19ff6940b86cdb4dac3e90b9409b36fb8d1
Hoang Nguyen <hnpn334@gmail.com>
2020-10-02 23:49:40 -0500

    Template

---
+++ src/main/java/com/hoang/jiraclonebe/JiraCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.hoang.jiraclonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraCloneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraCloneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoang/jiraclonebe/JiraCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoang.jiraclonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraCloneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
