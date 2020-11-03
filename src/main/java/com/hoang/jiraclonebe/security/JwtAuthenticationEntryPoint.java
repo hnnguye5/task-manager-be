491e5dac6cf8d5a7d05c8e1c50bbca3a9154c467
Hoang Nguyen <hnpn334@gmail.com>
2020-11-03 10:18:12 -0600

    Added login user error messages

---
+++ src/main/java/com/hoang/jiraclonebe/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.hoang.jiraclonebe.security;
+
+import com.google.gson.Gson;
+import com.hoang.jiraclonebe.exception.InvalidLoginResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// calls this method when user does not have access
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        // display error response in this current format
+        String jsonLoginResponse = new Gson().toJson(loginResponse);
+
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setStatus(401);
+        httpServletResponse.getWriter().print(jsonLoginResponse);
+    }
+}
