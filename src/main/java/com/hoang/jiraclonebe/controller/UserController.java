3d84d8da8d95e6b4d14c0570b89b478f2cc781c7
Hoang Nguyen <hnpn334@gmail.com>
2020-11-07 21:17:08 -0600

    Generated Token

--- src/main/java/com/hoang/jiraclonebe/controller/UserController.java
+++ src/main/java/com/hoang/jiraclonebe/controller/UserController.java
@@ -1,12 +1,20 @@
 package com.hoang.jiraclonebe.controller;
 
 import com.hoang.jiraclonebe.domain.User;
+import com.hoang.jiraclonebe.payload.JWTLoginSuccessResponse;
+import com.hoang.jiraclonebe.payload.LoginRequest;
+import com.hoang.jiraclonebe.security.JwtTokenProvider;
 import com.hoang.jiraclonebe.service.MapErrorValidation;
 import com.hoang.jiraclonebe.service.UserService;
 import com.hoang.jiraclonebe.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.security.Security;
+
+import static com.hoang.jiraclonebe.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,6 +38,31 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = mapErrorValidation.errorMapValidation(result);
+        if(errorMap != null)return errorMap;
+
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
